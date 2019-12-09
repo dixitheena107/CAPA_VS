5d32f80c47bc21d8b9459a84a39dcbab19fbfc2d
Heena Dixit <47897907+dixitheena107@users.noreply.github.com>
2019-12-09 10:28:14 -0500

    First Method Chrome

---
+++ VS_Selenium/VS_Selenium/FirstTestClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome; // Added Explicitly
+
+using System.Text;
+
+namespace VS_Selenium
+{
+    [TestClass]
+    public class FirstTestClass
+    {
+        // private IWebDriver driver;
+
+        [TestMethod]
+        public void TheBulletinChromeMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            // To maximize Window and close it
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://bulletin.uga.edu/");
+            driver.FindElement(By.Name("majors")).Click();
+            driver.FindElement(By.Id("ddlAllMajors")).Click();
+            new SelectElement(driver.FindElement(By.Id("ddlAllMajors"))).SelectByText("African American Studies - A.B.");
+            driver.FindElement(By.Id("ddlAllMajors")).Click();
+            driver.FindElement(By.Id("Imgbut_go_offAllMajors")).Click();
+
+
+            // WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            // wait.Until(ExpectedConditions.ElementExists((By.Id("lnkBtnMajorReq"))).click();
+
+            new WebDriverWait(driver, TimeSpan.FromSeconds(20)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.Id("lnkBtnMajorReq")))).Click();
+
+
+            // driver.FindElement(By.Id("lnkBtnMajorReq")).Click();
+            //  = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.FindElement(By.Id("pnlMajorReqSummaryNoShow")).Click();
+            driver.FindElement(By.LinkText("Entrance Requirements for the Major")).Click();
+
+
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ VS_Selenium/VS_Selenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VS_Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VS_Selenium")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcbeb75b-bc62-4b92-a7c3-5e9fbbc02175")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
